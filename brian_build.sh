d32db801d4cd674cc30a181858a5fd677bd1d84f
Brian Schiller <bgschiller@gmail.com>
2017-09-29 14:12:50 -0600

    try to fix build

---
+++ brian_build.sh
@@ -0,0 +1,6 @@
+runestone build && touch build/thinkcspy/.nojekyll && \
+echo 'coding101.devetry.com' > build/thinkcspy/CNAME && \
+for ff in $(find build/thinkcspy/ -name '*.html'); do
+    mkdir -p $(sed s/\.html$//g <<< $ff)
+    cp $ff $(sed s/\.html$//g <<< $ff)/index.html
+done
